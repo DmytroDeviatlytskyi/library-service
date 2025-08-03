7bff1fa0215b51ecca38245c60c63860d67255b4
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-08-03 17:51:51 +0200

    Fix REST_FRAMEWORK in settings & add Authorize header name instead of Authorization

--- library_service/settings.py
+++ library_service/settings.py
@@ -127,13 +127,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
 
-DJANGO_REST_FRAMEWORK = {
+REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
 }
 
 SIMPLE_JWT = {
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
