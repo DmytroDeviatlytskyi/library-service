6bfdbf1453f22d4822ad7296d3b21afe0804c21c
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-08-04 13:53:28 +0200

    Add borrowing create serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction, IntegrityError
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
@@ -18,3 +19,33 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "user", "book", "borrow_date", "expected_return_date")
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "expected_return_date")
+
+    def validate(self, data):
+        book = data["book"]
+        if book.inventory < 1:
+            raise serializers.ValidationError(
+                {"detail": f"Book `{book.title}` does not have in inventory"}
+            )
+        return data
+
+    def create(self, validated_data):
+        book = validated_data.pop("book")
+        try:
+            with transaction.atomic():
+                book.inventory -= 1
+                book.save()
+                borrowing = Borrowing.objects.create(
+                    book=book,
+                    **validated_data,
+                )
+                return borrowing
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"detail": f"User has already borrowed book `{book.title}`"}
+            )
